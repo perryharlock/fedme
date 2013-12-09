c0fc1c856f4da0cab71b518f3be87f7e9da557a3
Rowan Manning <accounts@rowanmanning.co.uk>
2013-12-09 18:22:10 +0000

    Add a bunch of FED images

--- fedme.js
+++ fedme.js
@@ -9,7 +9,21 @@ module.exports = {
 
     // FED image URLs
     things: [
-        'http://i.imgur.com/Tfq7Bi7.gif'
+        'http://i.imgur.com/1IpgJ8j.jpg',
+        'http://i.imgur.com/873UUyQ.jpg',
+        'http://i.imgur.com/aZNXXDQ.jpg',
+        'http://i.imgur.com/bdW4hXx.jpg',
+        'http://i.imgur.com/khW3ahU.jpg',
+        'http://i.imgur.com/pPGuifS.jpg',
+        'http://i.imgur.com/s5fHyVW.jpg',
+        'http://i.imgur.com/Tfq7Bi7.gif',
+        'http://i.imgur.com/UZyrj3N.jpg',
+        'http://i.imgur.com/wKnaB4C.jpg',
+        'http://i.imgur.com/zRclqdO.jpg',
+        'http://kil.gr/sandbox/secret/fedbot/jude.jpg',
+        'http://kil.gr/sandbox/secret/fedbot/judepad.jpg',
+        'http://kil.gr/sandbox/secret/fedbot/perrycheese.gif',
+        'http://www.gyford.com/images/jude.jpg'
     ]
 
 };
